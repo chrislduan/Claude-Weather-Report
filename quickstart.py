920344bad8869577c895a7b0bdb19b9026867e86
Christopher Duan <chrislduan@utexas.edu>
2025-11-05 13:33:46 -0600

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -1,7 +1,6 @@
 import anthropic
 
 client = anthropic.Anthropic()
-#export ANTHROPIC_API_KEY='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'
 
 
 message = client.messages.create(
